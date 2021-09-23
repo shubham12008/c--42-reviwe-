d81bace08f7707e721b11da4a5408c7cc0e3729a
shubham12008 <80684205+shubham12008@users.noreply.github.com>
2021-09-23 16:53:31 +0530

    Add files via upload

---
+++ review.js
@@ -0,0 +1,41 @@
+var test = 0;
+var name ="shubam";
+var score;
+var code = createSprite(200,random(0,500),50,100);
+var a1=[1111,"shubham",true,"12jcxfjdk"];
+console.log(a1[0]);
+a1.push(1522,1521);
+a1.pop();
+//hi 
+function shubham ();
+function shubham (){
+    console.log( "hi player ");
+
+
+}
+function setup(){
+    createCanvas(500,1200);
+}
+function draw(){
+    
+    test=test+1
+    text("score"+score,500,100);
+}
+if (test === 0){
+    console.log ("it is 0")
+}else {
+console.log ( "grater than 0" )
+}
+for (var i=20 ; i< 100 ; i++ ){
+    console.log(i);
+}
+class reconnect  {
+    constructor(x,y){
+
+    }
+    stopshare()
+    }
+var obj1 = new reconnect()
+// ref is refering to the data base
+//on listerning to date 
+// get set 
